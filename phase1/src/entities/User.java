de1a72b2967741982f2517420b4b501c790902ca
Your Name <you@example.com>
2020-11-02 10:15:17 -0500

    user entity // commented out line 11-15

--- phase1/src/entities/User.java
+++ phase1/src/entities/User.java
@@ -3,15 +3,16 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class User{
+public abstract class User {
     private String username;
     private boolean isLogin;
     private String password;
     private List<String> contactsList;
-<<<<<<< HEAD
-    String userID; // TODO: Make sure it implements java.io (DUE NOV 03)
-=======
->>>>>>> 34d9ed5048616df2ebb79f4c756ec34d332eb8f6
+
+//<<<<<<< HEAD
+//    String userID; // TODO: Make sure it implements java.io (DUE NOV 03)
+//=======
+// >>>>>>> 34d9ed5048616df2ebb79f4c756ec34d332eb8f6
 
 
     public User (String username, String password, List<String> contactsList, boolean isLogin){
