f9edd24e456032ade470c1d594d5b4edf7b86e8f
shencynt <cynt.shen@mail.utoronto.ca>
2020-11-07 13:36:11 -0500

    added methods to roomActions

--- phase1/src/useCase/RoomActions.java
+++ phase1/src/useCase/RoomActions.java
@@ -6,7 +6,32 @@ import entities.*;
 import useCase.GenerateID;
 
 public class RoomActions {
-    public HashMap<String, List<Room>> roomsList;
+    private HashMap<String, Room> roomsList; //TODO i feel like this should be private - isnt it a good design practice to encapsulate as much as possible? will think over later
+
+    //TODO i feel like this use case needs a constructor...otherwise how would we access the same hashmap unless it is static (and instructors strongly advice against static variables) - will think over later though, not sure
+    // (can also delete this constructor later. i was thinking that we may have to read in a csv/database via controller/gateway and store it in this hashmap)
+    public RoomActions(HashMap<String, Room> roomsList){
+        this.roomsList = roomsList;
+    }
+
+    public boolean addRoom(Room room){
+
+        if (roomsList.containsKey(room.getRoomId())){
+            return false;
+        }
+        roomsList.put(room.getRoomId(), room);
+        return true;
+    }
+
+    public boolean removeRoom(Room room){
+        if (roomsList.containsKey(room.getRoomId())){
+            roomsList.remove(room.getRoomId(), room);
+            return true;
+        }
+        return false;
+    }
+
+
 
 
 }
