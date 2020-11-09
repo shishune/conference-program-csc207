35578ff327926fe47e0bd9c53a7adc800fc95f18
tiechu <jiessie.tie@mail.utoronto.ca>
2020-11-09 16:07:40 -0500

    added controller classes

---
+++ phase1/src/controller/scheduleController.java
@@ -0,0 +1,19 @@
+package controller;
+
+public interface scheduleController {
+}
+/*    Parent BASIC USER type actions **make into interfaces according to action**
+         (user)
+        Send and receive messages
+        Add contacts
+        Delete/Block Contact
+        View Message
+
+        (schedule)
+        Sign-up for events
+        Cancel Spot in Event
+        View their own schedule (s)
+        View available schedule(s)
+        Number of spots left
+        Check conflict with timing and spots
+*/
