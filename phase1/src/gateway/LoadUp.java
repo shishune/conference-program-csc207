cdecb8ca1a13250e3947d1a65f57956c394d738a
Akiki Liang <akiki.liang@gmail.com>
2020-11-09 17:36:08 -0500

    Added IGateway method + MessageAction method templates

--- phase1/src/gateway/LoadUp.java
+++ phase1/src/gateway/LoadUp.java
@@ -1,24 +1,50 @@
 package gateway;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class LoadUp implements LoadUpIGateway {
-    ArrayList<String> messages = null;
+
+    private ArrayList<String> messages = null;
     // ArrayList<String> events = null;
     // ArrayList<String> organizers = null;
     // etc.etc. one for every .csv
 
     public LoadUp() {
+        // these might be moved elsewhere, idk yet
         getMessages();
         // getEvents()
         // getOrganizers()
         // etc. etc., one for every .csv
     }
     public List<String> getMessages() {
+        // gets list of messages from messages.csv and sets it to <messages>
+        try (BufferedReader br = new BufferedReader(new FileReader("../assets/dataFiles/messages.csv"))) {
+            String line = null;
+            while((line = br.readLine()) != null) {
+                messages.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return messages;
+    }
 
-        // gets list of messages from messages.csv
-        // and sets it to <messages>
+    // Overloaded with custom file path
+    public List<String> getMessages(String filePath) {
+        // gets list of messages from messages.csv and sets it to <messages>
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = null;
+            while((line = br.readLine()) != null) {
+                messages.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return messages;
     }
 
--- phase1/src/useCase/MessageActions.java
+++ phase1/src/useCase/MessageActions.java
@@ -1,13 +1,32 @@
 package useCase;
 
+import entities.Event;
+import gateway.LoadUp;
 import entities.Message;
-import useCase.GenerateID;
 
-// TODO: Ask about serialization, storage, and transfering to database in phase 2
+import java.util.HashMap;
+import java.util.List;
+
 // TODO: Add appropriate return types for methods
 // TODO: Add all required methods
 
 public class MessageActions {
+    private List<String> conversations = null;
+    private List<String> userMessages = null;
+
+    public HashMap<String, Message> messages;
+
+    private LoadUp loader = new LoadUp(); // this is okay because IGateway
+
+    /** gets list of messages from the IGateway **/
+    private void getAllConversations() {
+        conversations = loader.getMessagesList();
+    }
+
+    /** gets list of all messages between two users **/
+    private void getMessages(String senderId, String receiverId) {
+        // might not be necessary
+    }
 
     /** Gets a ___ of users for the speaker, organizer and attendee messaging actions
      **/
@@ -18,7 +37,40 @@ public class MessageActions {
         // TODO: Set return type to Message
     }
 
+
+    //=======================================
+
     /**
+     * Send message to a specific user
      **/
-    public void loadMessages() {}
+    public void sendMessage(String senderId, String receiverId) {
+
+    }
+
+    /**
+     * Send messages to multiple users
+     **/
+    public void sendMessage(String senderId, List<String> receiverIds) {
+        // check senderId for permission to send to multiple users
+    }
+
+    public void sendMessageToEvent() {
+
+    }
+
+    /**
+     * Returns nested array of all conversations for one user
+     **/
+    public void printMessages(String senderId) {
+        // presenter should call this method and turn array into output
+    }
+
+    /**
+     * Returns an array of conversation between one user and another user
+     **/
+    public void printMessages(String senderId, String receiverId) {
+        // presenter should call this method and turn array into output
+    }
+
+
 }
