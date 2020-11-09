0a1bfc77fba2310052090b595ccc34f6a3882841
Akiki Liang <akiki.liang@gmail.com>
2020-11-09 16:06:57 -0500

    Added template for loadup methods

--- phase1/src/gateway/LoadUp.java
+++ phase1/src/gateway/LoadUp.java
@@ -1,4 +1,54 @@
 package gateway;
 
-public class LoadUp {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadUp implements LoadUpIGateway {
+    ArrayList<String> messages = null;
+    // ArrayList<String> events = null;
+    // ArrayList<String> organizers = null;
+    // etc.etc. one for every .csv
+
+    public LoadUp() {
+        getMessages();
+        // getEvents()
+        // getOrganizers()
+        // etc. etc., one for every .csv
+    }
+    public List<String> getMessages() {
+
+        // gets list of messages from messages.csv
+        // and sets it to <messages>
+        return messages;
+    }
+
+    /** public List<String> getEvents() {
+
+     // gets list of events from events.csv
+     // and sets it to <events>
+     // return events
+     }
+     * **/
+
+    /** public List<String> getOrganizers() {
+
+     // gets list of organizers from organizers.csv
+     // and sets it to <organizers>
+     // return organizerss
+     }
+     * **/
+
+    /** etc. etc. one for every thingy
+     * **/
+
+    // Methods from IGateway
+    // To be called inside Use Case Classes
+    // this makes it okay to call a gateway inside use case class for some reason
+    @Override
+    public List<String> getMessagesList() {
+        return messages;
+    }
+
+    /** etc. etc. one for every thingy
+     * **/
 }
---
+++ phase1/src/gateway/LoadUpIGateway.java
@@ -0,0 +1,18 @@
+package gateway;
+
+import java.util.*;
+
+public interface LoadUpIGateway {
+    /** getEventsList()
+     * // gets list of events from the gateway
+     **/
+
+    /** getOrganizersList()
+     * // gets list of organizers from the gateway
+     **/
+
+    /** etc. etc. etc., one for every entity **/
+
+    public List<String> getMessagesList();
+
+}
