0c7200f926b5318a10b52d470bf4d400deb7dcc4
Eryka <31933725+shishune@users.noreply.github.com>
2020-11-10 17:53:00 -0500

    edit login

--- phase1/src/controller/OrganizerController.java
+++ phase1/src/controller/OrganizerController.java
@@ -2,8 +2,11 @@ package controller;
 
 public class OrganizerController {
     // TODO Cancel Event
+
     // TODO Create events
+
     // TODO  speakers
+
     // TODO Create rooms
     // TODO Schedule the speakers to each speak in one or more rooms at different times
     // TODO Reschedule events
