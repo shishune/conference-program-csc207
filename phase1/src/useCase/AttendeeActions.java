bf95a8ee7bbe70640d6ea19afb733d0f33c379b8
Suhyun Park <aodlfduddj@gmail.com>
2020-11-11 05:47:58 -0800

    edited AttendeeActions and OrganizerActions

--- phase1/src/useCase/AttendeeActions.java
+++ phase1/src/useCase/AttendeeActions.java
@@ -2,6 +2,7 @@ package useCase;
 
 import entities.Attendee;
 import entities.Event;
+import entities.Room;
 import entities.User;
 import useCase.GenerateID;
 
@@ -9,6 +10,12 @@ import java.util.HashMap;
 import java.util.List;
 
 public class AttendeeActions extends UserAccountActions{
+
+    private HashMap<String, Attendee> AttendeesHashMap;
+
+    public HashMap<String, Attendee> returnAttendeesHashMap(){
+        return AttendeesHashMap;
+    }
 
     public User createAttendee(String userId, String username, String password, List<String> contactsList, List<String> eventList, boolean isLogin) {
         Attendee userAttendee = new Attendee(userId, username, password, contactsList, eventList, isLogin, false);
--- phase1/src/useCase/OrganizerActions.java
+++ phase1/src/useCase/OrganizerActions.java
@@ -1,12 +1,19 @@
 package useCase;
 
+import entities.Event;
 import entities.Organizer;
 import entities.Speaker;
 import entities.User;
 
+import useCase.EventActions;
+import gateway.LoadUp;
+
+
 import java.util.List;
 
 public class OrganizerActions extends UserAccountActions {
+
+    private LoadUp loader = new LoadUp();
 
     public User createOrganizer(String userId, String username, String password, List<String> contactsList, List<String> eventList, boolean isLogin){
         Organizer userOrganizer = new Organizer(userId, username, password, contactsList, eventList, isLogin, false);
@@ -23,8 +30,36 @@ public class OrganizerActions extends UserAccountActions {
 
     }
 
-    // TODO: Event Actions stuff
-    //add new event object to the list of all event
-    //change timing of talks
-    //cancel events
+    public User createSpeaker(String userId, String username, String password, List<String> contactsList, List<String> eventList, boolean isLogin) {
+        Speaker userSpeaker = new Speaker(userId, username, password, contactsList, eventList, isLogin, false);
+        // addUserToHashMap(userSpeaker);
+        return userSpeaker;
+    }
+
+    public User createSpeaker(String username, String password, List<String> contactsList, List<String> eventList, boolean isLogin) {
+        GenerateID generateId = new GenerateID();
+        String userId = "S" + generateId;
+        Speaker userSpeaker = new Speaker(userId, username, password, contactsList, eventList, isLogin, false);
+        // addUserToHashMap(userSpeaker);
+        return userSpeaker;
+    }
+
+    public boolean addNewEvent(Event event) {
+        EventActions eventactions = new EventActions();
+        eventactions.loadEvent(event.getId(), event.getTitle(), event.getSpeaker(), event.getDateTime(),
+                event.getAttendees(), event.getRoomID());
+
+        List<String> eventList = loader.getEventsList();
+        return eventList.contains(event.getId());
+    }
+
+    public boolean changeEventTime(String eventId, String newDateTime) {
+        EventActions eventactions = new EventActions();
+        return eventactions.changeEventTime(eventId, newDateTime);
+    }
+
+    public List<String> cancelEvent(String eventId) {
+        EventActions eventactions = new EventActions();
+        return eventactions.cancelEvent(eventId);
+    }
 }
