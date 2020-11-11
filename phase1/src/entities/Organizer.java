6b55cf01bcb16930b0ef4adcabadba04d30864c7
Suhyun Park <aodlfduddj@gmail.com>
2020-11-11 05:49:58 -0800

    edited Attendee and Organizer

--- phase1/src/entities/Organizer.java
+++ phase1/src/entities/Organizer.java
@@ -6,40 +6,19 @@ import java.util.List;
 public class Organizer extends User{
     //private static String lastOrganizerIdNums = "";
 
+    /**
+     * Instantiates a new attendee object
+     * @param organizerId the string unique id of this organizer
+     * @param username the string username of this organizer
+     * @param password the string password required for organizer to login
+     * @param contactsList the list of people whom this organizer can message
+     * @param eventList the list of events which this organizer is scheduled to talk
+     * @param isLogin boolean whether this organizer has an account
+     * @param isOrganizer boolean whether this organizer is an organizer
+     * */
     public Organizer (String organizerId, String username, String password, List<String> contactsList, List<String> eventList, boolean isLogin, boolean isOrganizer){
         super(organizerId, username, password, contactsList, eventList, isLogin, isOrganizer); //added eventList to constructor (Jiessie) added userId to constructor
     }
-
-    /**
-     * Return a unique generated organizerID
-     * @return ID of an organizer
-     * */
-    /*public String generateId() {
-        String id = "O";
-        String lastOrganizerIdNumsSuffix;
-
-        if (lastOrganizerIdNums.equals("")) {
-            id = id + "0000000000000000";
-            lastOrganizerIdNums = "0000000000000000";
-        } else if (lastOrganizerIdNums.equals("0000000000000000")) {
-            id = id + "0000000000000001";
-            lastOrganizerIdNums = "0000000000000001";
-        } else {
-            lastOrganizerIdNumsSuffix = lastOrganizerIdNums.replaceAll("^[0]*", "");
-            System.out.println(lastOrganizerIdNumsSuffix);
-
-            int currRoomIdNumsSuffix = Integer.parseInt(lastOrganizerIdNumsSuffix) + 1;
-
-            for (int i = 0; i < 16 - Integer.toString(currRoomIdNumsSuffix).length(); i++) {
-                id += "0";
-            }
-            id += currRoomIdNumsSuffix;
-            lastOrganizerIdNums = id.substring(1);
-        }
-        System.out.println(id);
-        System.out.println(lastOrganizerIdNums);
-        return id;
-    }*/
 
     /**
      * Getter for the id unique to this organizer.
