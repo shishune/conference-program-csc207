a68789970a3b84f674348e4206ea6975af975929
jenjenjenjenjenvo <GitHub123>
2020-11-12 17:19:07 -0500

    coded logout

--- phase1/src/controller/LogOut.java
+++ phase1/src/controller/LogOut.java
@@ -1,5 +1,6 @@
 package controller;
 
+import entities.Attendee;
 import gateway.LoadUp;
 import gateway.LoadUpIGateway;
 import presenter.AccountPresenter;
@@ -7,11 +8,37 @@ import useCase.*;
 import gateway.Store;
 
 public class LogOut {
+    Store store;
+    MessageActions messageActions;
+    OrganizerActions organizerActions;
+    AttendeeActions attendeeActions;
+    RoomActions roomActions;
+    SpeakerActions speakerActions;
+    EventActions eventActions;
+
+    public LogOut(Store store, MessageActions messageActions, OrganizerActions organizerActions,
+                  AttendeeActions attendeeActions, RoomActions roomActions, SpeakerActions speakerActions,
+                  EventActions eventActions){
+        this.store = store;
+        this.messageActions = messageActions;
+        this.organizerActions = organizerActions;
+        this.attendeeActions = attendeeActions;
+        this.roomActions = roomActions;
+        this.speakerActions = speakerActions;
+        this.eventActions = eventActions;
+
+    }
     public void loggingOut() {
 
-        Store store = new Store();
-        UserAccountActions u = new UserAccountActions();
+        // Store store = new Store();
+        // UserAccountActions u = new UserAccountActions();
         // login procedure...
+        store.storeMessages(messageActions);
+        store.storeRooms(roomActions);
+        store.storeEvents(eventActions);
+        store.storeOrganizaers(organizerActions);
+        store.storeAttendees(attendeeActions);
+        store.storeSpeakers(speakerActions);
     }
 
     public void exit(){
