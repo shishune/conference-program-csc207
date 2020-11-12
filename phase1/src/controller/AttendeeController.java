bb5f5e910132b6264dcc1b858d2b9074cc0b2bd3
Suhyun Park <aodlfduddj@gmail.com>
2020-11-12 14:17:29 -0800

    added constructor for AttendeeController

---
+++ phase1/src/controller/AttendeeController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import entities.Attendee;
+import useCase.*;
+
+public class AttendeeController extends UserController{
+
+    public AttendeeController(UserAccountActions user, EventActions events, RoomActions rooms, MessageActions message, AttendeeActions attendee) {
+        super(user, events, rooms, message, attendee);
+    }
+
+
+}
