d6116d2e98a8b4891385844c05432357e69e2caf
Your Name <you@example.com>
2020-11-13 18:30:57 -0500

    javadic for LoginActions

--- phase1/src/useCase/LoginActions.java
+++ phase1/src/useCase/LoginActions.java
@@ -6,8 +6,20 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * A use case class which is responsible for validating of the user's username and password match
+ * @author Mizna & Jiessie
+ * @version 1
+ * */
+
 public class LoginActions {
 
+    /**
+     * Returns true when the username and password are correct otherwise returns false.
+     * @param username the username representing the username
+     * @param password the string representing the password
+     * @return A boolean if username and password are correct
+     * */
     public boolean isLogin(String username, String password) {
 
         UserAccountActions u = new UserAccountActions();
