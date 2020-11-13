e3c67695103412fcdc6448b9e33599b0e65f864c
Your Name <you@example.com>
2020-11-13 18:40:45 -0500

    javadoc for LoginController

--- phase1/src/controller/LogIn.java
+++ phase1/src/controller/LogIn.java
@@ -1,56 +1,20 @@
 package controller;
 
-import entities.Attendee;
 import entities.User;
-import gateway.LoadUp;
-import gateway.LoadUpIGateway;
-import entities.Organizer;
 import useCase.*;
-import presenter.AccountPresenter;
 
-import java.util.ArrayList;
 
 public class LogIn {
 
-/*    public MessageActions messageActions;
-    public EventActions eventActions;
-    public UserAccountActions userAccountActions;
-    public RoomActions roomActions;
-    public SpeakerActions speakerActions;
-    public OrganizerActions organizerActions;
-    public AttendeeActions attendeeActions;
-
-    // calls LoadUp
-    // calls next thingy; UserDashboard for example
-    // tada! done with LogIn stuff
-    // initialize all the usecase classes here
-
-    //Cynthia: made some edits to interact with presenter classes
-    public String loggingIn(String username, String password) {
-
-        LoginActions l = new LoginActions();
-        UserAccountActions u = new UserAccountActions();
-        AccountPresenter messages = new AccountPresenter();
-
-        LoadUpIGateway g = new LoadUp();
-        MessageActions messageActions = new MessageActions(g);
-        EventActions eventActions = new EventActions();
-        UserAccountActions userAccountActions = new UserAccountActions();
-        RoomActions roomActions = new RoomActions();
-        SpeakerActions speakerActions = new SpeakerActions();
-        OrganizerActions organizerActions = new OrganizerActions(g);
-        AttendeeActions attendeeActions = new AttendeeActions(g);
-
-        if (l.isLogin(username, password)) {
-            return u.usersHashMap.get(username).getId();
-        }
-
-        messages.failedLogin();
-        return null;
-
-    }*/
-
-    //alternative. let me know if you disagree
+    /**
+     * This method is called when the user is logging in after they have inputted a username and password and checks if
+     * the username and password are correct, if so returns the user object which was logging in, otherwise returns null
+     * @param username A string the user inputs as their username
+     * @param password Astring the user inputs as their password
+     * @param u A UserAccountActions object so it can access the use case class
+     * @return The user object of the user if the login was successful otherwise null if given username and password
+     * are incorrct.
+     */
     public User logIn(String username, String password, UserAccountActions u) {
         LoginActions l = new LoginActions();
         if (l.isLogin(username, password)) {
