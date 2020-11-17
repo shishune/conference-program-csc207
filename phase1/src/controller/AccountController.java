8d55063224c125cf81ae2d0d8e9c1f424f4e4fd9
shencynt <cynt.shen@mail.utoronto.ca>
2020-11-16 22:23:54 -0500

    minor edits

--- phase1/src/controller/AccountController.java
+++ phase1/src/controller/AccountController.java
@@ -87,55 +87,54 @@ public class AccountController {
                     menuController = (OrganizerMainMenuController)new OrganizerMainMenuController(user, organizerController);
                 }
 
-                //Menu
-                accountDisplay.printMainMenu();
-                String menuOption = scan.nextLine();
-                if (menuOption.equals("1")) {
-                    //logout procedure. will loop back to login procedure if user does not exit
-                    Store store = new Store();
-                    LogOut logOut = new LogOut(store, messageActions, organizerActions,
-                            attendeeActions, roomActions, speakerActions, eventActions, logoutActions);
+                while (true) {
+                    //Menu
+                    accountDisplay.printMainMenu();
+                    String menuOption = scan.nextLine();
+                    if (menuOption.equals("1")) {
+                        //logout procedure. will loop back to login procedure if user does not exit
+                        Store store = new Store();
+                        LogOut logOut = new LogOut(store, messageActions, organizerActions,
+                                attendeeActions, roomActions, speakerActions, eventActions, logoutActions);
 
-                    logOut.loggingOut(username, type);
+                        logOut.loggingOut(username, type);
 
-                    accountDisplay.successLogout();
-                    String choice = scan.nextLine();
-                    if (choice.equals("x") || choice.equals("X")) {
-                        logOut.exit();
+                        accountDisplay.successLogout();
+                        String choice = scan.nextLine();
+                        if (choice.equals("x") || choice.equals("X")) {
+                            logOut.exit();
+                        }
                         break;
-                    }
-                } else if (menuOption.equals("2")) {
-                    // send message. for attendee there is just one option, for organizer/speaker there
-                    // are several options
-                    menuController.option2();
-                } else if (menuOption.equals("3")) { //view all messages
-                    menuController.option3();
-                } else if (menuOption.equals("4")) { //add contact
-                    menuController.option4();
-                } else if (menuOption.equals("5")) { //view all contacts
-                    menuController.option5();
-                } else if (menuOption.equals("6")) {
-                    // attendee: sign up for event
-                    // organizer: add event
-                    // speaker: see schedule of given talks
-                    menuController.option6();
-                } else if (menuOption.equals("7") && (type.equals("A") || type.equals("O"))) {
-                    // attendee: cancel enrollment in event
-                    // organizer: remove event
+                    } else if (menuOption.equals("2")) {
+                        // send message. for attendee there is just one option, for organizer/speaker there
+                        // are several options
+                        menuController.option2();
+                    } else if (menuOption.equals("3")) { //view all messages
+                        menuController.option3();
+                    } else if (menuOption.equals("4")) { //add contact
+                        menuController.option4();
+                    } else if (menuOption.equals("5")) { //view all contacts
+                        menuController.option5();
+                    } else if (menuOption.equals("6")) {
+                        // attendee: sign up for event
+                        // organizer: add event
+                        // speaker: see schedule of given talks
+                        menuController.option6();
+                    } else if (menuOption.equals("7") && (type.equals("A") || type.equals("O"))) {
+                        // attendee: cancel enrollment in event
+                        // organizer: remove event
 
-                    menuController.option7();
+                        menuController.option7();
+                    } else if (menuOption.equals("8") && (type.equals("A") || type.equals("O"))) {
+                        menuController.option8();
+                    } else if (menuOption.equals("9") && (type.equals("A") || type.equals("O"))) {
+                        menuController.option9();
+                    } else {
+                        accountDisplay.printMenuError();
+                    }
+                    accountDisplay.promptReturn();
+                    scan.nextLine();
                 }
-                else if (menuOption.equals("8") && (type.equals("A") || type.equals("O"))){
-                    menuController.option8();
-                }
-                else if (menuOption.equals("9") && (type.equals("A") || type.equals("O"))){
-                    menuController.option9();
-                }
-                else {
-                    accountDisplay.printMenuError();
-                }
-                accountDisplay.promptReturn();
-                scan.nextLine();
             }
         }
     }
