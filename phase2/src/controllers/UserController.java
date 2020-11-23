0260852caa6667be224cbca47d0bb6cef07ad5bc
Your Name <you@example.com>
2020-11-23 16:22:49 -0500

    formatting usercontroller

--- phase2/src/controllers/UserController.java
+++ phase2/src/controllers/UserController.java
@@ -29,6 +29,7 @@ public class UserController {
     private HashMap<String, User> usernameHashmap = new HashMap<String, User>();
     private HashMap<String, User> userIdHashmap = new HashMap<String, User>();
 
+
     /**
      * Instantiates a new UserController object. Creates an instance of UserAccountActions, MessageActions, EventActions
      * AttendeeActions, RoomActions.
@@ -42,7 +43,7 @@ public class UserController {
             this.user = organizer;
         } else if (userType == 's') {
             this.user = speaker;
-        } else if (userType == 'a'){
+        } else if (userType == 'a') {
             this.user = attendee;
         }
 
@@ -50,8 +51,7 @@ public class UserController {
         this.organizerActions = organizer;
         this.speakerActions = speaker;
     }
-    //alternate constructor to access methods that do not need so many parameters
-//    public UserController(){};
+
 
     public HashMap<String, User> returnUserUsernameHashMap() {
         if (!(attendeeActions == null) && !attendeeActions.returnUsernameHashMap().isEmpty()) {
@@ -63,9 +63,6 @@ public class UserController {
         if (!(speakerActions == null) && !speakerActions.returnUsernameHashMap().isEmpty()) {
             usernameHashmap.putAll(speakerActions.returnUsernameHashMap());
         }
-//        if (!(user == null) && !user.returnUsernameHashMap().isEmpty()){
-////            usernameHashmap.putAll(speaker.returnSpeakerUsernameHashMap());
-////        }
         return usernameHashmap;
 
     }
@@ -84,14 +81,16 @@ public class UserController {
         return userIdHashmap;
     }
 
+
     /***
      * return if the username already belongs to a user
      * @param username
      * @return true if the username already exists
      */
-    public boolean usernameExists(String username){
+    public boolean usernameExists(String username) {
         return returnUserUsernameHashMap().containsKey(username);
     }
+
 
     /**
      * Sends a message to a user
@@ -107,7 +106,7 @@ public class UserController {
             // add receiver to contact list of sender
             user.addUserContactList(sender, receiver, usernameHash);
             user.addUserContactList(receiver, sender, usernameHash);
-            if(usernameHash.get(receiver) == null) {
+            if (usernameHash.get(receiver) == null) {
                 return false;
             }
             String receiverId = usernameHash.get(receiver).getId();
@@ -120,6 +119,7 @@ public class UserController {
         }
         return false;
     }
+
 
     /**
      * Adds a user to the contact list of another user
@@ -141,12 +141,6 @@ public class UserController {
         return user.removeUserContactList(toMe, removeMe);
     }
 
-//    public String viewMessageOneSender (String fromMe){
-//        MessageActions messageActions = new MessageActions();
-//        return messageActions.printMessages(fromMe).toString();
-//    };
-
-    //edited to work with presenter, let me know if you disagree
 
     /**
      * Shows the messages from one user to another
@@ -164,26 +158,25 @@ public class UserController {
             HashMap<String, User> userIdHash = returnUserIDHashMap();
             String receiverUsername = userIdHash.get(message.getReceiverId()).getUsername();
             String senderUsername = userIdHash.get(message.getSenderId()).getUsername();
-            if(senderUsername != null) {
+            if (senderUsername != null) {
                 info.add(senderUsername);
             }
-            if(receiverUsername != null){
+            if (receiverUsername != null) {
                 info.add(receiverUsername);
             }
-            if(message.getTimeSent() != null) {
+            if (message.getTimeSent() != null) {
                 info.add(message.getTimeSent());
             }
-            if(message.getMessage() != null){
+            if (message.getMessage() != null) {
                 info.add(message.getMessage());
             }
-            if(info.get(0) != null && info.get(1) != null && info.get(2) != null && info.get(3) != null){
+            if (info.get(0) != null && info.get(1) != null && info.get(2) != null && info.get(3) != null) {
                 messages.add(info);
             }
         }
         return messages;
     }
 
-    //need this for presenter
 
     /**
      * Shows the contacts of a user
@@ -203,7 +196,7 @@ public class UserController {
         }
         return contacts;
     }
-    //edited so that presenter can print fail messages based on why user cannot attend event
+
 
     /**
      * Adds an event to a user and an attendee to an event
@@ -248,6 +241,7 @@ public class UserController {
         return checks;
     }
 
+
     /**
      * Shows the events a given user is attending
      *
@@ -276,6 +270,7 @@ public class UserController {
         }
         return scheduleList;
     }
+
 
     /**
      * Shows the events a given user could be attending
@@ -310,6 +305,7 @@ public class UserController {
         return scheduleList;
     }
 
+
     /**
      * Shows the spots available in an event
      *
@@ -325,6 +321,7 @@ public class UserController {
         return r1.getCapacity() - eventActions.getEvent(eventID).getAttendees().size();
 
     }
+
 
     /**
      * Checks if users can attend an event or not
@@ -357,6 +354,7 @@ public class UserController {
         return false;
     }
 
+
     /**
      * Checks if event is full or not
      *
@@ -368,6 +366,7 @@ public class UserController {
 
     }
 
+
     /**
      * To be overridden by OrganizerController
      *
@@ -377,6 +376,7 @@ public class UserController {
     public boolean cancelEvent(String eventID) {
         return false;
     }
+
 
     /**
      * To be overridden by AttendeeController
@@ -415,92 +415,35 @@ public class UserController {
         return false;
     }
 
+
     /***
      * Check if event exists
      * @param eventName
      */
-    public boolean checkEvent(String eventName){
+    public boolean checkEvent(String eventName) {
         return (eventActions.getEventNames().containsKey(eventName));
     }
+
 
     /***
      * Check if event has attendees
      * @param eventName
      */
-    public boolean eventHasAttendees(String eventName){
+    public boolean eventHasAttendees(String eventName) {
         return (eventActions.getEventFromName(eventName).getAttendees().size() > 0);
     }
+
+
     /***
      * Check if any speakers exists in the system
      */
-    public boolean speakersExist(){
+    public boolean speakersExist() {
         return (speakerActions.returnIDHashMap().size() > 0);
     }
-
-
-
-
-//    // TODO ??? can this be here?
-//    public boolean leaveEvent(String eventName, String userId) {
-//        // String username = this.returnUserIDHashMap().get(userId).getUsername();
-//        // String username = this.attendee.returnUsersHashMap().get(userId).getUsername(); <-- original
-//        // i just took away the attendee in this.attendee but i wasnt sure if the 'this' was important
-//        // i chnged it because i made a master hashmap and the method is now in usercontroller not attendee
-//        String eventID = e.getEventFromName(eventName).getId();
-//        return this.attendee.removeEventFromUser(eventID, userId);
-//    }
 }
 
 
-//    public boolean cancelSpotEvent(String event, String user){
-//        AttendeeActions a = new AttendeeActions();
-//        User a1 = a.usersHashMap.get(user);
-//        String userId = a1.getId();
-//        if (userId.charAt(0) == 'A') {
-//            if () {
-//                EventActions e = new EventActions();
-//                e.events.get(event).removeAttendee(user);
-//
-//                Event e1 = e.events.get(event);
-//                AttendeeActions a = new AttendeeActions();
-//                User a1 = a.usersHashMap.get(user);
-//
-//                e.removeAttendee(e1.getId(), a1.getId());
-//                a1.getEventList().remove(event);
-//                return true;
-//            }
-//        }
-//
-//    };
-//ODO: organizers are the only ones that can cancel events (unfinished)
 
-//------------------------------------------
 
-//        Iterator<HashMap.Entry<String,Event>> it = e.events.entrySet().iterator();
-//        while (it.hasNext()){
-//            HashMap.Entry<String, Event> p = (HashMap.Entry <String, Event>)it.next();
-//            availableS.append(p);
-//            it.remove();
-//        }
-//        return availableS.toString();
-//ODO: this might not work, would rather use a list
-
-//        String availableEvents;
-//        for (int i = 0; i < allEvents.size(); i++) {
-//
-//            Event curr = e.events.get()
-
-//------------------------------------------
-
-/*      Send and receive messages
-        Add contacts
-        Delete/Block Contact
-        View Message
-        Sign-up for events
-        Cancel Spot in Event
-        View their own schedule (s)
-        View available schedule(s)
-        Number of spots left
-        Check conflict with timing and spots*/
 
 
