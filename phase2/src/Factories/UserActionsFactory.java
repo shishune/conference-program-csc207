a8817524f3eec7ee07a3ed351d1727b38747142a
shencynt <cynt.shen@mail.utoronto.ca>
2020-12-10 16:47:50 -0500

    Progress in factory design. (just an idea)

--- phase2/src/Factories/UserActionsFactory.java
+++ phase2/src/Factories/UserActionsFactory.java
@@ -1,4 +1,33 @@
 package Factories;
 
+import controllers.AttendeeController;
+import controllers.OrganizerController;
+import controllers.SpeakerController;
+import gateways.LoadUpIGateway;
+import useCases.*;
+
 public class UserActionsFactory {
+    public UserAccountActions getUserActions(String type, LoadUpIGateway loader) {
+
+        if(type == null){
+            return null;
+        }
+
+        if(type.equalsIgnoreCase("A")){
+            return new AttendeeActions(loader);
+        }
+
+        else if(type.equalsIgnoreCase("S")){
+            return new SpeakerActions(loader);
+        }
+
+        else if(type.equalsIgnoreCase("O")){
+            return new OrganizerActions(loader);
+        }
+
+        else
+        {
+            return null;
+        }
+    }
 }
