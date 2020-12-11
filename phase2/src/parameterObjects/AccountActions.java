5817a3f74add05c8b515ec5346daa765d7a3e877
shencynt <cynt.shen@mail.utoronto.ca>
2020-12-11 00:05:42 -0500

    updated parameter object classes.

--- phase2/src/parameterObjects/AccountActions.java
+++ phase2/src/parameterObjects/AccountActions.java
@@ -1,4 +1,33 @@
 package parameterObjects;
+import useCases.AttendeeActions;
+import useCases.SpeakerActions;
+import useCases.OrganizerActions;
 
+/**
+ * Parameter object for all use cases involving using the account to access the system of events.
+ * This includes account actions for attendees, speakers, and organizers.
+ */
 public class AccountActions {
+    private AttendeeActions attendeeActions;
+    private SpeakerActions speakerActions;
+    private OrganizerActions organizerActions;
+
+    public AccountActions(AttendeeActions attendeeActions, SpeakerActions speakerActions, OrganizerActions organizerActions){
+        this.attendeeActions = attendeeActions;
+        this.speakerActions = speakerActions;
+        this.organizerActions = organizerActions;
+    }
+
+    public AttendeeActions getAttendeeActions(){
+        return this.attendeeActions;
+    }
+
+    public SpeakerActions getSpeakerActions(){
+        return this.speakerActions;
+    }
+
+    public OrganizerActions getOrganizerActions(){
+        return this.organizerActions;
+    }
+
 }
--- phase2/src/parameterObjects/EventSystemActions.java
+++ phase2/src/parameterObjects/EventSystemActions.java
@@ -1,4 +1,41 @@
 package parameterObjects;
+import useCases.ConferenceActions;
+import useCases.EventActions;
+import useCases.MessageActions;
+import useCases.RoomActions;
 
+/**
+ * Parameter object for all use case classes involved in the system of events. This includes
+ * messages, conferences, rooms, and the events themselves.
+ */
 public class EventSystemActions {
+
+    private ConferenceActions conferenceActions;
+    private EventActions eventActions;
+    private MessageActions messageActions;
+    private RoomActions roomActions;
+
+    public EventSystemActions(ConferenceActions conferenceActions, EventActions eventActions, MessageActions messageActions, RoomActions roomActions){
+        this.conferenceActions = conferenceActions;
+        this.eventActions = eventActions;
+        this.messageActions = messageActions;
+        this.roomActions = roomActions;
+    }
+
+    public ConferenceActions getConferenceActions() {
+        return conferenceActions;
+    }
+
+    public EventActions getEventActions() {
+        return eventActions;
+    }
+
+    public MessageActions getMessageActions() {
+        return messageActions;
+    }
+
+    public RoomActions getRoomActions() {
+        return roomActions;
+    }
+
 }
