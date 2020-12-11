b2cfcba8ca77c9d09857b5b08cc48507a9f1a1fb
shencynt <cynt.shen@mail.utoronto.ca>
2020-12-11 01:09:01 -0500

    added documentation for parameter objects

--- phase2/src/parameterObjects/AccountActions.java
+++ phase2/src/parameterObjects/AccountActions.java
@@ -6,26 +6,46 @@ import useCases.OrganizerActions;
 /**
  * Parameter object for all use cases involving using the account to access the system of events.
  * This includes account actions for attendees, speakers, and organizers.
+ * Note that this is an extensible design that makes it more convenient to add more types of accounts
+ * instead of editing all the parameter lists. (It is also more readable.)
  */
 public class AccountActions {
     private AttendeeActions attendeeActions;
     private SpeakerActions speakerActions;
     private OrganizerActions organizerActions;
 
+    /**
+     * Instantiates the parameter object with all the use case objects that handle the account.
+     * @param attendeeActions use case for attendee account
+     * @param speakerActions use case for speaker account
+     * @param organizerActions use case for organizer account
+     */
     public AccountActions(AttendeeActions attendeeActions, SpeakerActions speakerActions, OrganizerActions organizerActions){
         this.attendeeActions = attendeeActions;
         this.speakerActions = speakerActions;
         this.organizerActions = organizerActions;
     }
 
+    /**
+     * getter for attendee use case
+     * @return attendee use case
+     */
     public AttendeeActions getAttendeeActions(){
         return this.attendeeActions;
     }
 
+    /**
+     * getter for speaker use case
+     * @return speaker use case
+     */
     public SpeakerActions getSpeakerActions(){
         return this.speakerActions;
     }
 
+    /**
+     * getter for organizer use case
+     * @return organizer use case
+     */
     public OrganizerActions getOrganizerActions(){
         return this.organizerActions;
     }
